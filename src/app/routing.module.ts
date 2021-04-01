3b95dc3b5d635c64a144b4e717d53bca4757b470
Fred Morrison <7603753+fmorriso@users.noreply.github.com>
2021-04-01 14:23:02 -0400

    reformat code

--- src/app/routing.module.ts
+++ src/app/routing.module.ts
@@ -4,26 +4,31 @@ import { HashLocationStrategy, LocationStrategy } from '@angular/common';
 
 const DEMO_APP_ROUTES: Routes = [
   { path: '', redirectTo: 'docs', pathMatch: 'full' },
-  { path: 'grid', loadChildren: () => import('./grid/grid.module').then(m => m.DocsGridModule) },
-  { path: 'docs', loadChildren: () => import('./layout/layout.module').then(m => m.DocsLayoutModule) },
+  { path: 'grid', loadChildren: () => import('./grid/grid.module').then((m) => m.DocsGridModule) },
+  {
+    path: 'docs',
+    loadChildren: () => import('./layout/layout.module').then((m) => m.DocsLayoutModule),
+  },
   {
     path: 'responsive',
-    loadChildren: () => import('./responsive/responsive.module').then(m => m.DocsResponsiveModule)
+    loadChildren: () =>
+      import('./responsive/responsive.module').then((m) => m.DocsResponsiveModule),
   },
   {
     path: 'issues',
-    loadChildren: () => import('./github-issues/github-issues.module').then(m => m.DocsGithubIssuesModule)
+    loadChildren: () =>
+      import('./github-issues/github-issues.module').then((m) => m.DocsGithubIssuesModule),
   },
   {
     path: 'stackoverflow',
-    loadChildren:
-      () => import('./stack-overflow/stack-overflow.module').then(m => m.DocsStackOverflowModule)
-  }
+    loadChildren: () =>
+      import('./stack-overflow/stack-overflow.module').then((m) => m.DocsStackOverflowModule),
+  },
 ];
 
 const routeOptions: ExtraOptions = {
-    enableTracing: true,
-    relativeLinkResolution: 'legacy'
+  enableTracing: true,
+  relativeLinkResolution: 'legacy',
 };
 
 @NgModule({
@@ -32,8 +37,8 @@ const routeOptions: ExtraOptions = {
   providers: [
     {
       provide: LocationStrategy,
-      useClass: HashLocationStrategy
-    }
-  ]
+      useClass: HashLocationStrategy,
+    },
+  ],
 })
 export class RoutingModule {}
